2531bb162254d89fa5beb8440eaea4ec316641c7
mxtaChilz <128243492+mxtaChilz@users.noreply.github.com>
2024-02-24 09:17:10 -0500

    Commit1

---
+++ src/test/java/org/bubz/databaseDemo/DatabaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bubz.databaseDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
